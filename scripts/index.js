7b96c67166aeb12abf565b2eaaa1a60b25a64934
Margarita Shakhova <mg.shakhova@yandex.ru>
2024-11-28 15:31:48 +0300

    done: добавлены функции создания и удаления карточки, функция выведения карточек на страницу

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardsContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard({name, link}, deleteCard) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const imageElement = cardElement.querySelector('.card__image');
+  const titleElement = cardElement.querySelector('.card__title');
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+
+  titleElement.textContent = name;
+  imageElement.src = link;
+  imageElement.alt = name;
+
+  deleteButton.addEventListener('click', () => deleteCard(cardElement));
+
+  return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(card) {
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function renderCards() {
+  initialCards.forEach(({name, link}) => {
+    cardsContainer.append(addCard({name, link}, deleteCard));
+  })
+}
+
+renderCards();
